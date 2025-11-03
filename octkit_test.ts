7e9ebbc83181e8ad2830e913b376b66dc807647d
ysknsid25 <kengo071225@gmail.com>
2025-11-03 20:26:40 +0900

    fix: リトライ処理の追加
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- octkit_test.ts
+++ octkit_test.ts
@@ -2,17 +2,22 @@ import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
 import { Octokit } from "https://esm.sh/@octokit/core";
 import { fetchGitHubUsers, User } from "./octkit.ts";
 
+// deno-lint-ignore no-explicit-any
 const createMockOctokit = (responses: any[]) => {
   let callCount = 0;
   const octokit = new Octokit();
+  // deno-lint-ignore no-explicit-any
   (octokit as any).graphql = async (
     query: string,
     options?: any,
   ): Promise<any> => {
     console.log(`MockOctokit.graphql call #${callCount + 1}`);
-    const response = responses[callCount];
+    const responseOrError = responses[callCount];
     callCount++;
-    return Promise.resolve(response);
+    if (responseOrError instanceof Error) {
+      return Promise.reject(responseOrError);
+    }
+    return Promise.resolve(responseOrError);
   };
   return octokit;
 };
@@ -69,3 +74,24 @@ Deno.test(
     assertEquals(users[0].login, "user1");
   },
 );
+
+Deno.test("fetchGitHubUsers - should retry on failure and succeed", async () => {
+  // Arrange
+  const mockResponses = [
+    new Error("502 Bad Gateway"), // 1回目の呼び出しは失敗
+    { // 2回目の呼び出しで成功
+      search: {
+        edges: [{ node: { login: "user1" } }],
+        pageInfo: { endCursor: "cursor-1", hasNextPage: false },
+      },
+    },
+  ];
+  const mockOctokit = createMockOctokit(mockResponses);
+
+  // Act
+  const users: User[] = await fetchGitHubUsers({ octokit: mockOctokit });
+
+  // Assert
+  assertEquals(users.length, 1);
+  assertEquals(users[0].login, "user1");
+});
